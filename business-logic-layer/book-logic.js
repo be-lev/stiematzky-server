cf8c381baef5d6c222430356c56259c8d1353d81
Lev <levberger19890@gmail.com>
2021-01-30 17:19:40 +0200

    add BLL

---
+++ business-logic-layer/book-logic.js
@@ -0,0 +1,48 @@
+const dal = require("../data-access-layer/dal");
+
+async function getAllBooksAsync() {
+  const sql = `SELECT bookId, G.genre, name, description , price , stock  
+    FROM  books  AS B 
+    JOIN genres as G 
+    on B.genre = G.genre`;
+
+  const books = await dal.executeAsync(sql);
+  return books;
+}
+
+async function addBookAsync(book) {
+  const sql = `INSERT INTO BOOKS VALUES(
+        DEFAULT,
+        '${book.genre}',
+        '${book.name}',
+        '${book.description}',
+        ${book.price},
+        ${book.stock})`;
+    const info = await dal.executeAsync(sql);
+    book.bookId = info.insertId;
+
+    book.name= await getBookType(book.genre)
+
+    return book
+}
+
+async function getBookType(genre) {
+    const sql = `SELECT bookType 
+    FROM genres 
+    WHERE genre = ${genre}`;
+    const genres = await dal.executeAsync(sql);
+    return genres[0].bookType
+}
+
+async function getAllGeneresAsync() {
+    const sql = `SELECT * FROM genres`
+    const genres = await dal.executeAsync(sql);
+    return genres
+}
+
+module.exports = {
+    getAllBooksAsync,
+    addBookAsync,
+    getAllGeneresAsync
+
+}
